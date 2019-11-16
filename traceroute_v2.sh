ffe74a67f9118502b1cdfc3689f7238b550298a8
Miiruki <aguilar.flavien@gmail.com>
2019-11-16 23:24:05 +0100

    test des TTL un par unù

---
+++ traceroute_v2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cibles=( "-T -p 80" "-T -p 22" "-T -p 443" "-I")
+read -p 'Veuillez entrer une adresse IP ou un nom de domaine : ' destination
+while [ -z "$destination" ]; do
+    read -p 'Veuillez rentrer une adresse IP ou un nom de domaine :' destination
+done
+TTL_Max=$(traceroute -n $destination | sed -n '$p' | awk '{print $1}')
+TTL=$(traceroute -n $destination | sed '1d' | awk '{print $1}')
+for (( TTL=1; TTL<=$TTL_Max ; TTL++ )); do
+for i in "${cibles[@]}"; do
+    cmd=$(traceroute -n -A -m $TTL -f $TTL -q 1 -w 10 $i $destination | awk '{print $1,$2}'| sed -e's/(//g' | sed -e's/)//g' | sed '1d')
+    echo "$cmd"
+
+done
+done
